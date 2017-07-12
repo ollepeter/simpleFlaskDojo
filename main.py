755a5b2f3770a78f5ea25285317e33e90b3730f7
ollepeter <mr.peterolle@gmail.com>
2017-07-12 22:12:38 +0200

    add 'name' variable to the URL and render it

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return 'Hello Flask'
 
+
+@app.route('/hello/<name>')
+def hello():
+    print(name)
+    return 'Hello {0} !'.format(name)
+
 if __name__ == '__main__':
     app.run(debug=True)
