06802b2f6b272ec7513d750ee90ff61b130e48f5
ollepeter <mr.peterolle@gmail.com>
2017-07-13 08:55:09 +0200

    test the trailing slash in the URL

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ def hello(name):
     print(name)
     return 'Hello {0} !'.format(name)
 
+
+@app.route('/square/<int:num>')
+def double(num):
+    square = num * num
+    return 'The square of {0} is {1}!'.format(num, square)
+
+
+@app.route('/flask/')
+def flask():
+    return 'This is /flask/...'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
