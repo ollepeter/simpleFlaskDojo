f279e9c74a839eccb63e251e8dde421f9e1abc1f
ollepeter <mr.peterolle@gmail.com>
2017-07-13 14:14:45 +0200

    implement HTTP requests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -41,6 +41,34 @@ def check_admin(name):
         return redirect(url_for('greet_admin'))
     else:
         return redirect(url_for('greet_guest', guest=name))
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Hello {0} !'.format(name)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        print('000 FORM 000')
+        print(request.form)     
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.arg.get['nm']
+        return redirect(url_for('success', name=user))
+
+
+@app.route('/genre', methods=['POST'])
+def genre():
+        genre = request.form['gender']
+        values = request.values
+        print('000 VALUES 000')
+        print(request.values)
+        print('000 FORM 000')
+        print(request.form)     
+        return values['gender']
 
 
 if __name__ == '__main__':
