62fbfd4937212271aea5a35218818e1b72e726c0
ollepeter <mr.peterolle@gmail.com>
2017-07-13 15:36:28 +0200

    implement usage of javascript file in static folder

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/hello/<int:score>')
-def hello_name(score):
-    return render_template('hello.html', marks=score)
-
-
-@app.route('/result')
-def result():
-    dict = {'phy': 50,
-            'che': 60,
-            'maths': 70
-            }
-    return render_template('result.html', result=dict)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
