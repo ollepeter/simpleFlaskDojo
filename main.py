52c97c98fb95202733b7cc1bea5abd56a2e5d292
ollepeter <mr.peterolle@gmail.com>
2017-07-13 16:20:44 +0200

    implement a small web-app with a POST request

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def student():
+    return render_template('student.html')
 
+
+@app.route('/result', methods=['POST'])
+def result():
+    results = request.form
+    print(results)
+    return render_template('result.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
