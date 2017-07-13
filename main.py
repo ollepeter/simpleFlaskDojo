6ddac566339b408453adcda94dd4794cd16318f2
ollepeter <mr.peterolle@gmail.com>
2017-07-13 09:16:44 +0200

    use 'url_for' for URL building

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -23,6 +23,24 @@ def double(num):
 @app.route('/flask/')
 def flask():
     return 'This is /flask/...'
+
+
+@app.route('/admin')
+def greet_admin():
+    return 'Hello Admin!'
+
+
+@app.route('/guest/<guest>')
+def greet_guest(guest):
+    return 'Hello {0} as guest'.format(guest)
+
+
+@app.route('/user/<name>')
+def check_admin(name):
+    if name == 'admin':
+        return redirect(url_for('greet_admin'))
+    else:
+        return redirect(url_for('greet_guest', guest=name))
 
 
 if __name__ == '__main__':
