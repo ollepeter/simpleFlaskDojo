2965f16a39255f4980031bcfa4f725d70e5d0afe
ollepeter <mr.peterolle@gmail.com>
2017-07-13 15:02:23 +0200

    implement for loop using jinja2

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ def hello_name(score):
     return render_template('hello.html', marks=score)
 
 
+@app.route('/result')
+def result():
+    dict = {'phy': 50,
+            'che': 60,
+            'maths': 70
+            }
+    return render_template('result.html', result=dict)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
